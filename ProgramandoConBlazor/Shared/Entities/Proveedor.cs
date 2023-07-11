39cb4f8d379c0df9092d7cc393f87113d2eb8f1e
Alejandro Elias <alejandro.elias@outlook.com>
2023-07-10 21:08:16 -0600

    Add project files.

---
+++ ProgramandoConBlazor/Shared/Entities/Proveedor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramandoConBlazor.Shared.Entities
+{
+    public class Proveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+    }
+}
